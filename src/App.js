8929b41843d0cce623ec373cb0f556633e66c1f4
David Evbodaghe <david.evbodaghe@aiesec.net>
2020-06-02 23:45:35 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import GetLocation from './GetLocation'
+
+function App() {
+  return (
+    <GetLocation />
+  );
+}
+
+export default App;
---
+++ src/GetLocation.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class GetLocation extends Component{
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          console.log("Latitude is :", position.coords.latitude);
+          console.log("Longitude is :", position.coords.longitude);
+        });
+      }
+    
+    
+    render(){
+        return(
+            <div> Current Location coordinates are ... </div>
+
+        )
+    }
+}
+
+export default GetLocation
