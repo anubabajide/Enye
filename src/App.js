14a0554cb1ce2fdf8235c758162cf87753b6edf3
David Evbodaghe <david.evbodaghe@aiesec.net>
2020-06-03 14:17:20 +0100

    static map issue with api key

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component{
     this.state = {
         latitude : null,
         longitude: null,
+        userAddress:""
     };
     this.getLocation = this.getLocation.bind(this);
     this.getCoordinates = this.getCoordinates.bind(this);
@@ -27,16 +28,44 @@ class App extends React.Component{
       longitude:position.coords.longitude
     })
   }
+
+   handleError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.")
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.")
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.")
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.")
+        break;
+        default:
+        alert("Unknown error")
+    }
+  }
   render(){
     return(
-      <div>
-      <div>Testing Location</div>
-      <h2>
-        <button onClick = {this.getLocation}> go</button>
+      <div className = "App"> 
+        <h2>
+          Testing Location
+        </h2>
+        <button onClick = {this.getLocation}> Get Location</button>
+        <h4>Coordinates</h4>
         <p>Latitude : {this.state.latitude}</p>
         <p>Longitude : {this.state.longitude}</p>
-      </h2>
-      <GetLocation /></div>
+        <h4>User Address is : {this.state.userAddress}}</h4>
+        // {
+        //   this.state.latitude && this.state.longitude ?
+        //   <img src = {`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.latitude},${this.state.longitude}&zoom=14&size=400x300&sensor=false&key=${}&markers=color:red%7C${this.state.latitude},${this.state.longitude}&key=${AIzaSyDLBm-g5esokD5x2NLshJ-Io5lCqK_nrtM}`} alt = '' />
+        //   :
+        //   null
+        // }*/
+      </div>
+      
 
     );
   }
